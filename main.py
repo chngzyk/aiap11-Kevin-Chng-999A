f97ed7836b93b7032344ed0903733a80db4c0c8b
chngzyk <chngzyk@gmail.com>
2022-06-13 17:43:50 +0800

    test

---
+++ main.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def main():
+
+    logger = logging.getLogger(__name__)
+
+    logger.info("Running machine learning pipeline")
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
